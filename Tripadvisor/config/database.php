5ba096831fab647a0ab831d570f15732472e423c
Aeziak <aeziak@gmail.com>
2018-10-02 06:16:39 +0000

    Second Commit on Branch Commit2

--- Tripadvisor/config/database.php
+++ Tripadvisor/config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,14 +56,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'g231'),
+            'username' => env('DB_USERNAME', 'g231'),
+            'password' => env('DB_PASSWORD', 'Yct2TRSs'),
             'charset' => 'utf8',
             'prefix' => '',
-            'schema' => 'public',
+            'schema' => 'tripadvisor',
             'sslmode' => 'prefer',
         ],
 
